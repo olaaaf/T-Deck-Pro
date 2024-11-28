d7fdc4b75d15035b0e450ec3a8f790b1da835d66
ShallowGreen123 <2608653986@qq.com>
2024-11-28 10:55:19 +0800

    fix pcm5102 test

--- examples/aduio_pcm5102a/Aduio_pcm5102a.ino
+++ examples/aduio_pcm5102a/Aduio_pcm5102a.ino
@@ -21,40 +21,8 @@
         Serial.printf("[%d] Execution error\n", __LINE__);
 
 Audio audio;
-Ticker ticker;
-struct tm timeinfo;
-time_t now;
 
-uint8_t hour = 6;
-uint8_t minute = 59;
-uint8_t sec = 45;
-
-bool f_time = false;
-int8_t timefile = -1;
-char chbuf[100];
 bool audio_paly_flag = false;
-
-void tckr1s()
-{
-    sec++;
-    if (sec > 59)
-    {
-        sec = 0;
-        minute++;
-    }
-    if (minute > 59)
-    {
-        minute = 0;
-        hour++;
-    }
-    if (hour > 23)
-    {
-        hour = 0;
-    }
-    if (minute == 59 && sec == 50)
-        f_time = true; // flag will be set 10s before full hour
-    Serial.printf("%02d:%02d:%02d\n", hour, minute, sec);
-}
 
 void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
 {
@@ -137,6 +105,7 @@ void sd_card_init(void)
 void setup()
 {
     Serial.begin(115200);
+
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
 
     sd_card_init();
@@ -145,58 +114,16 @@ void setup()
     ASSERT_FLAG(audio_paly_flag);
 
     audio.setVolume(21); // 0...21
-    ticker.attach(1, tckr1s);
+
+    delay(5000);
 
     pinMode(BOARD_6609_EN, OUTPUT);
     digitalWrite(BOARD_6609_EN, HIGH);
+
+    audio_paly_flag = audio.connecttoFS(SD, "/voice_time/BBIBBI.mp3");
 }
 
 void loop()
 {
     audio.loop();
-    if (f_time == true)
-    {
-        f_time = false;
-        timefile = 3;
-        uint8_t next_hour = hour + 1;
-        if (next_hour == 25)
-            next_hour = 1;
-        Serial.printf("Paly Audio\n");
-        audio_paly_flag = audio.connecttoFS(SD, "/voice_time/BBIBBI.mp3");
-        ASSERT_FLAG(audio_paly_flag);
-    }
-}
-
-void audio_eof_mp3(const char *info)
-{ // end of file
-    // Serial.printf("file :%s\n", info);
-    if (timefile > 0)
-    {
-        Serial.printf("timefile: %d\n", timefile);
-        if (timefile == 1)
-        {
-            audio_paly_flag = audio.connecttoFS(SD, "/voice_time/2-on.mp3");
-            ASSERT_FLAG(audio_paly_flag);
-            timefile--;
-        } // stroke
-        if (timefile == 2)
-        {
-            audio_paly_flag = audio.connecttoFS(SD, "/voice_time/2-on.mp3");
-            ASSERT_FLAG(audio_paly_flag);
-            timefile--;
-        } // precisely
-        if (timefile == 3)
-        {
-            audio_paly_flag = audio.connecttoFS(SD, "/voice_time/2-on.mp3");
-            ASSERT_FLAG(audio_paly_flag);
-            timefile--;
-        }
-    }
-}
-
-// optional
-void audio_info(const char *info)
-{
-    Serial.print("info        ");
-    Serial.println(info);
 }
